d4ec1041f01787684e8fd14eae1b0b2791fb3a83
Joana-Mansa <joanaowusu279@gmail.com>
2020-11-25 07:54:48 +0000

    Day 5- HackerRank

---
+++ loops.py
@@ -0,0 +1,12 @@
+import math
+import os 
+import random
+import re
+import sys
+
+if __name__=='__main__':
+    n=int(input("Enter the number here: "))
+    for i in range(1,11):
+        x = n * i
+        2<= n and n<=20
+        print (n, 'x', i, '=', n*i)
